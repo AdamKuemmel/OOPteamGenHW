d5bb1282177c343fb7653da4335a16faf48ac3ca
AdamKuemmel <Adamkuemmel@gmail.com>
2021-08-30 23:15:01 -0600

    finished readme

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# OOPteamGenHW
+# ![N/A sheild](https://img.shields.io/badge/license-N%2FA-red) OOPteamGenHW
+
+## Description
+
+This project is used to generate a webpage that displays a users team and gives access to their basic info, emails, github profiles and office numbers
+
+-it uses jest to run tests
+
+-it uses Inquirer to collect input from the users
+
+-it uses classes to extend employees properties and methods to intern, manager and engineer sub classes
+
+-it uses validator to validator input from user is desired data type
+
+## Table of Contents
+
+-[Installation](#installation)
+
+-[Usage](#usage)
+
+-[Contribution](#contribution)
+
+-[Test](#test)
+
+-[Questions](#Questions)
+
+## Installation
+
+```
+npm i
+```
+
+## Test
+
+```
+npm tests
+```
+
+## Usage
+
+```
+node index.js
+```
+
+-answer all questions
+
+## License
+
+this project is not curently using any licensing
+
+## Contribution
+
+n/a
+
+## Questions
+
+Have more questions? please reach out via message at one of these provdided links
+
+github.com/adamkuemmel
+
+adamkuemmel@gmail.com
--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,11 +4,11 @@ describe("employee", () => {
   describe("Initialization", () => {
     it("should create an object with name, id and email", () => {
       //arrange    act
-      const emplo = new Employee("bruce", 666, "adamkuemmel@gmail.com");
+      const emplo = new Employee("bruce", 111, "adamkuemmel@gmail.com");
 
       //assert
       expect(emplo.name).toEqual("bruce");
-      expect(emplo.id).toEqual(666);
+      expect(emplo.id).toEqual(111);
       expect(emplo.email).toEqual("adamkuemmel@gmail.com");
     });
     it("should throw an err if provided no arguments", () => {
@@ -16,47 +16,47 @@ describe("employee", () => {
       expect(emp).toThrow();
     });
     it("should throw an err if name isnt a string", () => {
-      const emp = () => new Employee(666, 666, "a@gmail.com");
+      const emp = () => new Employee(111, 111, "a@gmail.com");
       expect(emp).toThrow();
     });
     it("should throw an err if id isnt a number", () => {
-      const emp = () => new Employee("bruce", "666", "a@gmail.com");
+      const emp = () => new Employee("bruce", "111", "a@gmail.com");
       expect(emp).toThrow();
     });
     it("should throw an err if id is a negative number", () => {
-      const emp = () => new Employee("bruce", -666, "a@gmail.com");
+      const emp = () => new Employee("bruce", -111, "a@gmail.com");
       expect(emp).toThrow();
     });
 
     it("should throw an err if email isnt an email", () => {
-      const emp = () => new Employee("bruce", 666, 666);
+      const emp = () => new Employee("bruce", 111, 111);
       expect(emp).toThrow();
     });
   });
   describe("getName()", () => {
     it("should return the value of this.name", () => {
-      const name = new Employee("bruce", 666, "a@gmail.com");
+      const name = new Employee("bruce", 111, "a@gmail.com");
       const testName = name.name;
       expect(testName).toEqual("bruce");
     });
   });
   describe("getId()", () => {
     it("should return the value of tthis.id", () => {
-      const id = new Employee("bruce", 666, "a@gmail.com");
+      const id = new Employee("bruce", 111, "a@gmail.com");
       const testId = id.id;
-      expect(testId).toEqual(666);
+      expect(testId).toEqual(111);
     });
   });
   describe("getEmail()", () => {
     it("should return the value of this.name", () => {
-      const email = new Employee("bruce", 666, "a@gmail.com");
+      const email = new Employee("bruce", 111, "a@gmail.com");
       const testEmail = email.email;
       expect(testEmail).toEqual("a@gmail.com");
     });
   });
   describe("getRole()", () => {
     it("should return the value of this.name", () => {
-      const role = new Employee("bruce", 666, "a@gmail.com");
+      const role = new Employee("bruce", 111, "a@gmail.com");
       const testRole = role.getRole();
       expect(testRole).toEqual("employee");
     });
